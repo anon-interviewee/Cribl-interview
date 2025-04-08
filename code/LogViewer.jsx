178a8fe02f2f0e3306cbba75698922e0adb35f2d
Mike Yoon <mike.yoon@observeinc.com>
2025-04-07 23:52:17 -0700

    Fix codesandbox issue and optimize rendering performance

--- code/LogViewer.jsx
+++ code/LogViewer.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { fetchLogs, prettyPrint } from "./utils.jsx";
 
+const CHUNK_SIZE = 1000;
+
 export const LogViewer = () => {
   const [logs, setLogs] = React.useState([]);
   const [openRows, setOpenRows] = React.useState(new Set());
@@ -26,36 +28,58 @@ export const LogViewer = () => {
     );
   }, []);
 
-  return (
-    <div className="grid">
-      <div className="header">
+  // Divides logs into chunks for performance
+  const chunked = React.useMemo(() => {
+    const result = [];
+    for (var i = 0; i < logs.length; i += CHUNK_SIZE) {
+      result.push(logs.slice(i, i + CHUNK_SIZE));
+    }
+
+    if (i % CHUNK_SIZE !== 0) {
+      result.push(logs.slice(i));
+    }
+
+    return result;
+  }, [logs])
+
+  const renderChunk = (chunk, index) => {
+    const startIndex = index * CHUNK_SIZE;
+
+    return <div className="grid">
+      {index === 0 ? <div className="header">
         <div></div>
         <div>Time</div>
         <div>Event</div>
-      </div>
-      
-      {logs.map((log, i) => {
-        if (openRows.has(i)) {
+      </div> : null}
+      {chunk.map((log, i) => {
+        const rowIndex = startIndex + i;
+        if (openRows.has(rowIndex)) {
           return (
-            <React.Fragment key={i}>
+            <React.Fragment key={rowIndex}>
               <div className="row no-border">
-                <div className="caret" onClick={() => onToggleRow(i)}><span className="open">&gt;</span></div>
-                <div key={i + "time"}>{new Date(log._time).toISOString()}</div>
-                <div key={i + "event"}></div>
+                <div className="caret" onClick={() => onToggleRow(i)}>
+                  <span className="open">&gt;</span>
+                </div>
+                <div>{new Date(log._time).toISOString()}</div>
+                <div></div>
               </div>
               <pre className="content">{prettyPrint(log)}</pre>
             </React.Fragment>
           );
         } else {
           return (
-            <div className="row" key={i}>
-              <div className="caret" onClick={() => onToggleRow(i)}>&gt;</div>
-              <div key={i + "time"}>{new Date(log._time).toISOString()}</div>
-              <div key={i + "event"}>{JSON.stringify(log)}</div>
+            <div className="row" key={rowIndex}>
+              <div className="caret" onClick={() => onToggleRow(i)}>
+                &gt;
+              </div>
+              <div>{new Date(log._time).toISOString()}</div>
+              <div>{JSON.stringify(log)}</div>
             </div>
           );
         }
       })}
     </div>
-  );
+  }
+
+  return chunked.map((chunk, i) => renderChunk(chunk, i))
 };
